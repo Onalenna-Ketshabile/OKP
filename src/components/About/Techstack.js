5e5262e6ab62c9038218e40e76a3ef40f49b1be2
Onalenna Ketshabile <onalenna.ketshabile@atos.net>
2024-03-08 08:57:20 +0200

    added items to TechStack

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -9,8 +9,14 @@ import {
   DiGit,
   DiJava,
   DiMysql,
+  DiHtml5,
+  DiCss3,
+  DiAngularSimple,
 } from "react-icons/di";
 import {
+  SiCsharp,
+  SiCss3,
+  SiHtml5,
   SiPostgresql,
 } from "react-icons/si";
 
@@ -20,6 +26,7 @@ function Techstack() {
       <Col xs={4} md={2} className="tech-icons">
         <CgCPlusPlus />
       </Col>
+
       <Col xs={4} md={2} className="tech-icons">
         <DiJava />
       </Col>
@@ -45,6 +52,17 @@ function Techstack() {
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <DiPython />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiHtml5 />
+      </Col>
+
+      <Col xs={4} md={2} className="tech-icons">
+        <DiCss3 />
+      </Col>
+
+      <Col xs={4} md={2} className="tech-icons">
+        <DiAngularSimple />
       </Col>
  
     </Row>
