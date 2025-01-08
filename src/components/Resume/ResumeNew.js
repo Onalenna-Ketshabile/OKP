25e708db348e17aa392aa26812a55aa542520f15
Onalenna Ketshabile <220150475@student.uj.ac.za>
2025-01-08 23:27:11 +0200

    Update ResumeNew.js

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "./../../Assets/Onalenna Ketshabile Resume 2024.pdf";
+import pdf from "./../../Assets/Onalenna Ketshabile.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
